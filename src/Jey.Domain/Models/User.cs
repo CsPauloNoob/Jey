eb31796f41aa649d75cbee790dfe544a2aa60545
CsPauloNoob <ps616131@gmail.com>
2023-01-14 23:56:08 -0500

    Adicionar arquivos de projeto.

---
+++ src/Jey.Domain/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jey.Domain.Models
+{
+    public class User
+    {
+        public ulong UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        public uint Jcoin { get; set; }
+
+        public uint Scoin { get; set; }
+    }
+}
